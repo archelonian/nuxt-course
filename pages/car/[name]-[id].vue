83f8aa23ffe18fca38c5e5e866d66d17cb42dcbb
Bonnie Chen <bonnie.chen.077@gmail.com>
2024-04-03 18:50:45 -0400

    Use nuxt layouts

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,26 +1,11 @@
 <template>
   <div>
-    <NavBar />
-    <div class="
-      mx-auto
-      mt-4
-      max-w-7xl
-      space-y-4
-      px-4
-      xs:px-8
-      sm:px-10
-      lg:px-16
-      pb-16
-      w-3/5"
-    >
-      <div>
-        <CarDetailHero />
-        <CarDetailAttributes />
-        <CarDetailDescription />
-        <CarDetailContact />
-      </div>
+    <div>
+      <CarDetailHero />
+      <CarDetailAttributes />
+      <CarDetailDescription />
+      <CarDetailContact />
     </div>
-
   </div>
 </template>
 
@@ -28,5 +13,9 @@
 const route = useRoute()
 useHead({
   title: route.params.name
+})
+
+definePageMeta({
+  layout: "custom"
 })
 </script>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,22 +1,8 @@
 <template>
   <div>
-    <NavBar />
-    <div class="
-      mx-auto
-      mt-4
-      max-w-7xl
-      space-y-4
-      px-4
-      xs:px-8
-      sm:px-10
-      lg:px-16
-      pb-16
-      w-3/5"
-    >
-      <div class="mt-32 flex">
-        <CarSidebar />
-        <NuxtPage />
-      </div>
+    <div class="mt-32 flex">
+      <CarSidebar />
+      <NuxtPage />
     </div>
   </div>
 </template>
@@ -25,5 +11,9 @@
 const route = useRoute()
 useHead({
   title: `${route.params.make ? route.params.make + 's' : 'Cars'} in ${route.params.city}`
+})
+
+definePageMeta({
+  layout: "custom"
 })
 </script>
