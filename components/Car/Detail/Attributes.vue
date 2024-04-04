c4dc637c83cb9f2db8408a57d8f9de185d0d05df
Bonnie Chen <bonnie.chen.077@gmail.com>
2024-04-03 23:52:06 -0400

    Install tailwind css and adjust app to work again

--- components/Car/Detail/Attributes.vue
+++ components/Car/Detail/Attributes.vue
@@ -2,7 +2,7 @@
   <div class="mr-10 mt-5 border-b pb-5">
     <div class="flex text-lg mt-2">
       <p class="rounded text-lime-800 mr-3">&gt;</p>
-      <p>All exterior lights function</p>
+      <p>All exterior lights currently functional</p>
     </div>
   </div>
 </template
--- components/Car/Hero.vue
+++ components/Car/Hero.vue
@@ -6,14 +6,15 @@
     bg-no-repeat
     bg-cover
     bg-bottom
-    bg-[url]('https://carconfigurator.ferrari.com/assets/cars/portofinom/packages/default/car-ferrari-portofino-m_splash.jpg')"
+    bg-[url('https://www.topgear.com/sites/default/files/2023/11/McLaren_750S_TheDrive-0847.jpg?w=1784&h=1004')]"
+
   >
     <div class="z-10 absolute w-full h-full flex justify-center items-center">
       <div class="text-center w-1/2">
         <h1 class="text-8xl font-serif mb-10 text-white">Find your drive.</h1>
         <CarSearchBar />
       </div>
-      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
     </div>
+    <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
   </div>
 </template>
